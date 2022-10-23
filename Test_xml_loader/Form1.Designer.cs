e7d613389a39d6809ab98e5959df620e15060c38
Brad <bsoverns@gmail.com>
2022-10-23 16:19:36 -0400

    First push

---
+++ Test_xml_loader/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Test_xml_loader
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadDataGrid = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadDataGrid
+            // 
+            this.btnLoadDataGrid.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadDataGrid.Name = "btnLoadDataGrid";
+            this.btnLoadDataGrid.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadDataGrid.TabIndex = 1;
+            this.btnLoadDataGrid.Text = "Load Grid";
+            this.btnLoadDataGrid.UseVisualStyleBackColor = true;
+            this.btnLoadDataGrid.Click += new System.EventHandler(this.btnLoadDataGrid_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(185, 126);
+            this.Controls.Add(this.btnLoadDataGrid);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadDataGrid;
+    }
+}
+
---
+++ Test_xml_loader/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+using System.Xml;
+using System.Timers;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace Test_xml_loader
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        public static string stockCode;
+        
+        private void btnLoadDataGrid_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string serverUrl = @"http://in.finance.yahoo.com/d/quotes.csv?s=FB" +
+                    //"&f=sl1d1t1c1ohgvj1pp2owern&e=.csv"; //original line
+                        //"&f=sl1d1t1c1ohgvj1pp2owerk1&e=.csv"; //original line
+                        "&f=aa2a5bb2b3b4b6cc1c3c6c8dd1d2ee1e7e8e9f6gg1g3g4g5g6hii5jj1j3j4j5j6kk1k2k3k4k5ll1l2l3mm2m3m4m5m6m7m8nn4opp1p2p5p6qrr1r2r5r6r7ss1s7t1t6t7t8vv1v7ww1w4xy&e=.csv"; //original line
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(serverUrl);
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                StreamReader reader = new StreamReader(response.GetResponseStream(),
+                    Encoding.ASCII);
+
+                string stockDataString = reader.ReadLine();
+                //string[] stockDataContents = stockDataString.Split(',');
+
+                string[] stockDataContents = stockDataString.Split(',');
+                
+                var items = from item in stockDataContents
+                                        select item;
+
+                foreach (string s in items)
+                {
+                    MessageBox.Show(s);
+                }
+
+                //for (int i = 0; i <= 1; i++)
+                //{
+                //    MessageBox.Show(stockDataContents[i].ToString());
+                //}
+
+                //[stockDataContents.Rows.Count, stockDataContents.Columns.Count];
+
+                response.Close();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("An Error Occurred\nException Message: " + ee.Message);
+            }
+        }
+    }
+}
